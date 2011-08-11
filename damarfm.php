d2f576c24dd9c6151122314c8bd034995f6fa993
DamarFm <DamarFm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-11 07:21:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/damarfm-player/trunk@422031 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- damarfm.php
+++ damarfm.php
@@ -17,11 +17,11 @@ function widget_DamarFm_Com($args) {
 ?>
         <?php echo $before_widget; ?>
 
- <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="300" height="250" id="MainTimeline" align="middle">
+ <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="300" height="236" id="MainTimeline" align="middle">
 <param name="allowScriptAccess" value="sameDomain" />
 <param name="allowFullScreen" value="false" />
-<param name="movie" value="http://yayin.damarfm.com/damaraacp.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#141414" />
-<embed src="http://yayin.damarfm.com/damaraacp.swf" quality="high" bgcolor="#141414" width="300" height="250" name="wpembed" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+<param name="movie" value="damaraacp_wp.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#141414" />
+<embed src="damaraacp_wp.swf" quality="high" bgcolor="#141414" width="300" height="236" name="wpembed" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
 </object>
 <center><a href="http://www.damarfm.com"><font size="1px">DamarFm.Com Player</font></a></center>
 
--- readme.txt
+++ readme.txt
@@ -10,7 +10,7 @@ Requires at least: 2.6
 
 Tested up to: 3.1.1
 
-Stable tag: 1.0
+Stable tag: 1.1
 
 
 
@@ -46,7 +46,9 @@ Yok.
 
 == Changelog ==
 
+= 1.1 =
 
+* Design Yenilendi
 
 = 1.0 =
 
